48b08cefc48b7b3747627506e0ff359511bf1822
jedicodeman <lci2021003@iiitl.ac.in>
2022-04-08 19:20:16 +0530

    api

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/admin", adminRoutes);
-app.use("/student", studentRoutes);
-app.use("/teacher", teacherRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/teacher", teacherRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("Page not found");
